d150eaa060bc92eeb4157e7991c973cad0fee7e6
Anthony Lam <Anthony Lam>
2017-05-18 21:21:23 -0700

    Fix bit twiddling for mailbox code

--- can_bus_driver/arduino_can_bus/src/can_bus.c
+++ can_bus_driver/arduino_can_bus/src/can_bus.c
@@ -8,32 +8,44 @@
 #define READ_STATUS 0xFB
 #define GET_MESSAGE 0xEF
 #define SEND_MESSAGE 0xEE
+#define CHECK_MESSAGE 0xED
 
 
 void read_mailbox_message(tCAN *message)
 {
-    // Read in id
-    message->id = 0;
-    message->id |= (uint8_t)MAILBOX_DATA_ADDR(0);
-    message->id |= (uint8_t)(MAILBOX_DATA_ADDR(1)<<4);
+    uint32_t word;
+    word = MAILBOX_DATA(0);
+    message->id = (word >> 24) & 0xFF;
+    message->id |= (word >> 16) & 0xFF;
+    message->header.rtr = (word >> 8) & 0xFF;
+    message->header.length = word;
 
-    message->header.rtr = (int8_t)MAILBOX_DATA_ADDR(2);
-    message->header.length = (uint8_t)MAILBOX_DATA_ADDR(3);
-
-    for(int i = 0; i < 8; ++i) {
-        message->data[i] = MAILBOX_DATA_ADDR(4+i);
+    for(int i=1; i>=0; --i) {
+        word = MAILBOX_DATA(i+1);
+        message->data[0+4*i] = (word >> 24) & 0xFF;
+        message->data[1+4*i] = (word >> 16) & 0xFF;
+        message->data[2+4*i] = (word >> 8) & 0xFF;
+        message->data[3+4*i] = word;
     }
 }
 
 void write_mailbox_message(tCAN *message)
 {
-    MAILBOX_DATA_ADDR(0) = (int8_t)message->id;
-    MAILBOX_DATA_ADDR(1) = (int8_t)(message->id>>4);
-    MAILBOX_DATA_ADDR(2) = message->header.rtr;
-    MAILBOX_DATA_ADDR(3) = message->header.length;
+    uint8_t word[4];
+    // Assume big-endian right hurr
+    word[3] = (message->id>>8) & 0xFF; 
+    word[2] = (message->id) & 0xFF;
+    word[1] = message->header.rtr;
+    word[0] = message->header.length; 
 
-    for(int i = 0; i < 8; ++i) {
-        MAILBOX_DATA_ADDR(4+i) = message->data[i];
+    MAILBOX_DATA(0) = *(uint32_t *)word; 
+
+    for(int i=0; i<2; ++i) {
+        word[3] = message->data[0+4*i];
+        word[2] = message->data[1+4*i];
+        word[1] = message->data[2+4*i];
+        word[0] = message->data[3+4*i];
+        MAILBOX_DATA(i+1) = *(uint32_t *)word;
     }
 }
 
@@ -47,13 +59,16 @@ int main() {
                           D_GPIO,D_GPIO,D_GPIO,
                           D_GPIO,D_GPIO,D_SS,
                           D_MOSI,D_MISO,D_SPICLK);
+    uint8_t command;
+    tCAN *message;
 
     while(1) {
         // Command address is empty (ie no commands)
         // We might want to actually do some work while idle
         while(MAILBOX_CMD_ADDR == 0x00);
 
-        switch(MAILBOX_CMD_ADDR) {
+        command = MAILBOX_CMD_ADDR;
+        switch(command) {
             case RESET:
                 mcp2515_reset(MAILBOX_DATA(0));
                 break;
@@ -67,20 +82,23 @@ int main() {
                 mcp2515_bit_modify(MAILBOX_DATA(0),MAILBOX_DATA(1),MAILBOX_DATA(2));
                 break;
             case READ_STATUS:
-                mcp2515_read_status(MAILBOX_DATA(0));
+                MAILBOX_DATA(0) = mcp2515_read_status(MAILBOX_DATA(0));
                 break;
             case GET_MESSAGE:
-                tCAN *message = malloc(sizeof(tCAN));
+                message = malloc(sizeof(tCAN));
                 write_mailbox_message(message);
                 mcp2515_get_message(message);
                 free(message);
                 break;
             case SEND_MESSAGE:
-                tCAN *message = malloc(sizeof(tCAN));
+                message = malloc(sizeof(tCAN));
                 read_mailbox_message(message);
                 mcp2515_send_message(message);
                 free(message);
                 break;
+            case CHECK_MESSAGE:
+                // TODO Check message here
+                // XGpio_WriteData();
             default:
                 break;
         }
