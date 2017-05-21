1fdbc93b5c4a02d597e4ac07ccc38ae26150ea12
Anthony Lam <thelamacmdr@gmail.com>
2017-05-21 14:30:17 -0700

    Use pytest format

--- python/can/main.py
+++ python/can/main.py
@@ -1,10 +1,17 @@
+import pytest
 from pynq.iop.arduino_can_bus import Can,Message
 
 message = Message()
+message.id = 500
+message.rtr = 17
+message.length = 23
 message.data = [[1,2,3,4],[5,6,7,8]]
 
-c = Can()
-c._write_message(message)
-print(message)
-data = c._read_message()
-print(data)
+
+def test_mailbox_rw():
+    c = Can()
+    c._write_command(0)
+    c._write_message(message)
+    data = c._read_message()
+    assert str(data) == str(message), \
+        "data and message string mismatched"
