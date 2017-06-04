9eb7e9c13e3c92ab1ab3dc6fbe89a9af769e1931
Anthony Lam <thelamacmdr@gmail.com>
2017-06-04 14:22:13 -0700

    Improved controllability

--- python/can/test_ecu.py
+++ python/can/test_ecu.py
@@ -3,29 +3,68 @@ import OBD
 import pytest
 import time
 import logging
+from pynq.board import Switch
+from pynq.board import Button
 
 
 CANSPEED_125 = 7
 CANSPEED_250 = 3
 CANSPEED_500 = 1
+logging.basicConfig(filename="ecu_log.log", level=logging.INFO)
+obd = None
+en = True
 
-logging.basicConfig(filename="ecu_quick.log", level=logging.INFO)
-obd = OBD.OBD(CANSPEED_500, logger=False)
-counter = 0
-time.sleep(2)
 
-while True:
+def reset():
+    global obd
+    global en
+    canspeed = 0
+    speed_select0 = Switch(0)
+    speed_select1 = Switch(1)
+    val = speed_select1.read() << 1 | speed_select0.read()
+
+    en = True
+    if val == 0:
+        canspeed = CANSPEED_500
+    elif val == 1:
+        canspeed = CANSPEED_250
+    elif val == 2:
+        canspeed = CANSPEED_125
+    else:
+        en = False
+
+    logging.debug(canspeed)
+    obd = OBD.OBD(canspeed, logger=False)
+    time.sleep(2)
+
+
+def run(counter: int):
+    global obd
     if counter % 10 == 0:
         obd.ecu_req(constants.VEHICLE_SPEED)
-    elif counter % 10 == 1:
+    if counter % 10 == 1:
         obd.ecu_req(constants.THROTTLE)
-    elif counter % 10 == 2:
+    if counter % 10 == 2:
         obd.ecu_req(constants.ENGINE_RPM)
-    elif counter % 10 == 3:
+    if counter % 10 == 3:
         obd.ecu_req(constants.ENGINE_COOLANT_TEMP)
-    elif counter % 10 == 4:
+    if counter % 10 == 4:
         obd.ecu_req(constants.O2_VOLTAGE)
-    elif counter % 10 == 5:
+    if counter % 10 == 5:
         obd.ecu_req(constants.MAF_SENSOR)
 
-    counter += 1
+    time.sleep(0.5)
+
+if __name__ == "__main__":
+    rst = Button(0)
+    counter = 0
+    reset()
+
+    while True:
+        if rst.read():
+            logging.debug("Resetting")
+            reset()
+            counter = 0
+        elif en:
+            run(counter)
+            counter += 1
